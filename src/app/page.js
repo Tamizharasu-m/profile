62105ce119f330dbdccd1f28188b8f07dedcf5ef
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-26 15:09:02 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./Header/Header";
 import ProfileIntro from "./component/ProfileIntro/ProfileIntro";
+import About from "./component/About/About";
 
 export default function Home() {
   return (
     <div>
       <ProfileIntro/>
+      <About/>
     </div>
   );
 }
