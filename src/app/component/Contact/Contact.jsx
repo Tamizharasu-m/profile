f01a26c472bfca97f1288acb68224a768444e502
Tamizharasu-m <d.mtamizharasu25@gmail.com>
2024-08-26 16:00:52 +0530

    First commit

--- src/app/component/Contact/Contact.jsx
+++ src/app/component/Contact/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 function Contact() {
   return (
     <div>
-      
+      <div>
+        <Image
+        src={""}
+        width={500}
+        height={500}
+        className=''
+        />
+      </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
--- src/app/component/ProfileIntro/ProfileIntro.jsx
+++ src/app/component/ProfileIntro/ProfileIntro.jsx
@@ -5,10 +5,10 @@ import Header from "@/app/Header/Header";
 function ProfileIntro() {
   return (
     <div className="md:h-[100vh] relative ">
-      <div className="fixed top-[30px] left-0 right-0 z-[999]">
+      <div className="fixed top-[20px] left-0 right-0 z-[999]">
         <Header />
       </div>
-      <div className="md:flex w-[100%] md:items-center px-[15px] md:px-[25px] lg:px-[100px] relative top-[50px] lg:top-[130px]">
+      <div className="md:flex w-[100%] md:items-center px-[15px] md:px-[25px] lg:px-[100px] relative top-[80px] lg:top-[130px]">
         <div className="lg:w-[50%]">
           <h2 className="text-[46px] mb-[20px] lg:text-[60px]">
             Front-End React <br></br> Developer
